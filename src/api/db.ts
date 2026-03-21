cd39a302efc09f630da7ff1a97a8bb645f5d11b3
Peter John Rueca <peter.rueca@safetychampion.online>
2026-03-21 11:07:23 +0800

    setup sveltekit

--- src/api/db.ts
+++
@@ -1,5 +0,0 @@
-import { Database } from "bun:sqlite";
-import { BunSQLiteDatabase, drizzle } from "drizzle-orm/bun-sqlite";
-
-const sqliteDB = new Database(String(process.env.DB));
-export const db: BunSQLiteDatabase = drizzle(sqliteDB);
